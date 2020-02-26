d79de81599aa519aa960e88483b7c80b3cfef25b
Eric T. Dawson <eric.t.dawson@gmail.com>
2020-02-26 15:20:53 -0500

    Add normalize_sample_counts function

--- R/normalize_counts.R
+++ R/normalize_counts.R
@@ -9,3 +9,15 @@ normalize_counts <- function(x){
   }
   return (x)
 }
+
+#' @export
+normalize_sample_counts <- function(x){
+  if (sum(x$Amount) > 1){
+    x <- x %>%
+      dplyr::group_by(Sample) %>%
+      dplyr::mutate(tot = sum(Amount)) %>%
+      dplyr::mutate(Amount = Amount / tot) %>%
+      dplyr::select(-tot)
+  }
+  return (x)
+}
