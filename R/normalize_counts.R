5640b6b591ecba8e66aee991e57b961e4792a908
Eric T Dawson <eric.t.dawson@gmail.com>
2020-04-24 16:23:32 -0400

    Add functions for calculating y-limits intelligently; update namespace, man

--- R/normalize_counts.R
+++ R/normalize_counts.R
@@ -29,3 +29,66 @@ normalize_sample_counts <- function(x){
   }
   return (x)
 }
+
+#' Returns a set of ylimits trimmed to fit the input dataframe.
+#' @param x A TidySig tibble
+#' @param fitMinimum fit a minimum value instead of defaulting to zero.
+#' @return A vector of two elements containing calculated ylimits.
+#' @export
+calculate_smart_ylimits <- function(x, fitMinimum=FALSE){
+  min_y <- 0
+  if (fitMinimum){
+    min_y <- add_val_buffer(get_minimum_amount(x), buffer_pct = 0.1, ceilingBuf = FALSE, roundInteger = FALSE)
+  }
+  max_y <-add_val_buffer(get_minimum_amount(x), buffer_pct = 0.1, ceilingBuf = TRUE, roundInteger = FALSE)
+
+  return (c(min_y, max_y))
+}
+
+#' Add some amount of buffer to a value.
+#' Used to calculate the min/max amounts
+#' for ylimits
+#' @param val A numeric type.
+#' @param buffer_pct The proportion of val to add/subtract to/from val.
+#' @param ceilingBuf If true, add buffer_pct to val. Otherwise, subtract buffer_pct from val.
+#' @param roundInteger If true, return the nearest ceiling/floor integer to the buffered value.
+#' @return A numeric value (v +- buffer_pct * val)
+add_val_buffer <- function(val, buffer_pct=0.01, ceilingBuf=TRUE, roundInteger=FALSE){
+  stopifnot(buffer_pct < 1)
+  ret <- NULL
+  if (ceilingBuf){
+    ret <- val + (val * buffer_pct)
+    if (roundInteger){
+      ret <- ceiling(ret)
+    }
+  }
+  else{
+    ret <- max(0, val - (val * buffer_pct))
+    if (roundInteger){
+      ret <- floor(ret)
+    }
+  }
+  return(ret)
+}
+
+#' Retrieve the maximum value
+#' of a signature or activity dataframe.
+#' This function can be used to set intelligent y-limits
+#' in plotting functions.
+#' @param x a TidySig tibble
+#' @return A numeric with the highest Amount in the dataframe
+#' @export
+get_maximum_amount <- function(x){
+  return (max(x$Amount))
+}
+
+#' Retrieve the minimum value
+#' of a signature or activity dataframe.
+#' This function can be used to set intelligent y-limits
+#' in plotting functions.
+#' @param x a TidySig tibble
+#' @return A numeric with the lowest Amount in the dataframe
+#' @export
+get_minimum_amount <- function(x){
+  return (min(x$Amount))
+}
