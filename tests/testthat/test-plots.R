4f5874766a724aee971a8c2bf754f32375479356
Eric T. Dawson <eric.t.dawson@gmail.com>
2020-04-28 14:18:15 -0400

    Update plot tests

--- tests/testthat/test-plots.R
+++ tests/testthat/test-plots.R
@@ -2,7 +2,7 @@
 require("readr")
 x_sigs_sbs <- tidysig::transform_sigprofiler_df(read_tsv("test_sigs.tsv"))
 x_sigs_id83 <- tidysig::transform_sigprofiler_df(read_tsv("test_id83_sigs.tsv"))
-
+x_activities <- tidysig::transform_sigprofiler_df(read_tsv("test-activities.tsv"))
 
 test_that("plot_signature produces the expected plot for SBS96", {
   plot  <- plot_signature(x_sigs_sbs)
@@ -16,15 +16,16 @@ test_that("plot_signature produces the expected plot for ID83", {
 })
 
 test_that("[plot_signature,  ID83] can use smart y-limits", {
-  plot_dig  <- plot_signature(x_sigs_id83) %>% digest::digest(algo = "md5")
-  expect_equal(0, 0)
+  plot  <- plot_signature(x_sigs_id83, ylimits = "smart")
+  vdiffr::expect_doppelganger("ID83 w/ smart ylimits", plot)
 })
 
 test_that("[plot_signature,  SBS96] can use smart y-limits", {
-  plot_dig  <- plot_signature(x_sigs_id83) %>% digest::digest(algo = "md5")
-  expect_equal(0, 0)
+  plot  <- plot_signature(x_sigs_id83) %>% digest::digest(algo = "md5")
+  vdiffr::expect_doppelganger("SBS96 w/ smart ylimits", plot)
 })
 
 test_that("plot_activity produces the expected plot for SBS96", {
-  expect_equal(2 * 2, 4)
+  plot <- plot_activities(x_activities)
+  vdiffr::expect_doppelganger("Signature Activity plot", plot)
 })
